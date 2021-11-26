6d120178378cc13f01ff6d780ff82a33578cbdd0
Monk <61575072+MonkNo1@users.noreply.github.com>
2021-11-26 08:46:07 +0530

    index.php

--- api/index.php
+++ api/index.php
@@ -10,82 +10,24 @@
         private $db = NULL;
 
         public function __construct(){
-            parent::__construct();                // Init parent contructor
-            $this->db = Database::getConnection();                    // Initiate Database connection
+            parent::__construct();               
+            $this->db = Database::getConnection();                 
         }
-
-        /*
-         * Public method for access api.
-         * This method dynmically call the method based on the query string
-         *
-         */
+        
         public function processApi(){
             $func = strtolower(trim(str_replace("/","",$_REQUEST['rquest'])));
             if((int)method_exists($this,$func) > 0)
                 $this->$func();
             else
-                $this->response('',400);                // If the method not exist with in this class, response would be "Page not found".
+                $this->response('',400);                
         }
 
         /*************API SPACE START*******************/
-
-        private function about(){
-
-            if($this->get_request_method() != "POST"){
-                $error = array('status' => 'WRONG_CALL', "msg" => "The type of call cannot be accepted by our servers.");
-                $error = $this->json($error);
-                $this->response($error,406);
-            }
-            $data = array('version' => '0.1', 'desc' => 'This API is created by Blovia Technologies Pvt. Ltd., for the public usage for accessing data about vehicles.');
-            $data = $this->json($data);
-            $this->response($data,200);
-
-        }
 
         private function test(){
                 $data = $this->json(getallheaders());
                 $this->response($data,101);
         }
-
-        private function server(){
-            if($this->get_request_method() != "POST"){
-                $data = $this->json($_SERVER);
-                $this->response($data,200);
-            }
-            $data = [
-                    "status" => "unauthorized"
-            ];
-            $data = $this->json($data);
-            $this->response($data,401);
-        }
-
-        public function verifipls()
-        {
-            if($this->get_request_method() != "GET" and isset($this->_request['user']) and isset($this->_request['pass'])){
-                $user = $this->_request['user'];
-                $pass = $this->_request['pass'];
-                if($user === "monk" and $pass === "isthework"){
-                    $data = [
-                        "status" => "Login success"
-                    ];
-                    $data = $this->json($data);
-                    $this->response($data,200);
-                }
-                else{
-                     $data = [
-                         "status" => "Login Failed"
-                     ];
-                     $data = $this->json($data);
-                     $this->response($data,400);
-             }
-         }
-         $data = [
-             "status" => "unauthorized"
-         ];
-         $data = $this->json($data);
-         $this->response($data,401);
-     }
-
 
         /*************API SPACE END*********************/
 
